e5e938bf33843dcf3040700445cba1382c53ddac
salim100blaz <bourhimsalim22@gmail.com>
2026-01-06 13:11:38 +0100

    Update part1.ts

--- src/part1.ts
+++ src/part1.ts
@@ -1,29 +1,16 @@
-// Variable globale (effet de bord)
+
 let counter = 0;
 
-/**
- * Fonction pure :
- * - mêmes entrées => même sortie
- * - aucun effet de bord
- */
+
 export function add(a: number, b: number): number {
   return a + b;
 }
 
-/**
- * Fonction impure :
- * - dépend d’un état externe (counter)
- * - modifie cet état
- */
+
 export function increment(): number {
   counter++;
   return counter;
 }
 
-/*
-Pourquoi add est-elle prévisible ?
-→ Parce qu’elle ne dépend que de ses paramètres et ne modifie aucun état externe.
 
-Pourquoi increment ne l’est pas ?
-→ Parce qu’elle dépend d’une variable globale et change son état à chaque appel.
-*/
+
